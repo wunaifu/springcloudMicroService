f9e53ea9a9c1c266a004dfb028bdeb6b14806f6f
wunaifu <1278022159@qq.com>
2018-07-21 21:07:22 +0800

    hystrix熔断器

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/wnf/springcloud/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.wnf.springcloud.service;
+
+import com.wnf.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService
+{
+	public boolean add(Dept dept);
+
+	public Dept get(Long id);
+
+	public List<Dept> list();
+}
